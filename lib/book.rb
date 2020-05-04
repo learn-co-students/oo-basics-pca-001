bc0bb0a8b49d56b80b04a4dc0cb951b86dd4d36b
Aaron <adub65@gmail.com>
2020-05-04 16:32:43 -0400

    book and shoe

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,35 @@
+class Book
+  def initialize(title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
 
+  def author=(author)
+    @author = author
+    def author
+      @author
+    end
+  end
+
+  def page_count=(num)
+    @page_count = num
+    def page_count
+      @page_count
+    end
+  end
+
+  def genre=(genre)
+    @genre = genre
+    def genre
+      @genre
+    end
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
